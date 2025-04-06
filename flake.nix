fcfac9221d2f9e66d257e709055d2cb2fd0d257b
Robin Bate Boerop <me@robinbb.com>
2025-04-05 18:24:12 -0700

    Add devex tooling

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+
+      # used by nix shell and nix develop
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            git
+            nix
+            ruby
+          ];
+      };
+  });
+}
